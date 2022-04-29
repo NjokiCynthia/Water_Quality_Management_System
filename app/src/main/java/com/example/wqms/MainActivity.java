e95deca5c74a2a35a2f52844478573a8e8143b6c
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-04-29 21:02:02 +0300

    Set up login, signup and forgot password

--- app/src/main/java/com/example/wqms/MainActivity.java
+++ app/src/main/java/com/example/wqms/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.wqms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+    CardView card;
+    Button access;
+    TextView f_text, copyright;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo);
+        access = findViewById(R.id.access);
+        f_text = findViewById(R.id.f_text);
+        copyright = findViewById(R.id.copyright);
+
+        access.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,
+                    com.example.wqms.Authentication.LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
