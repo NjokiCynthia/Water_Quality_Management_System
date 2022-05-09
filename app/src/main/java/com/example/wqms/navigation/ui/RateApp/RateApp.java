87cbaf38fb3ba66fd9c37de1a58696b759d73cab
njokicynthia590@gmail.com <njokicynthia590@gmail.com>
2022-05-09 23:53:11 +0300

    Edit navigation drawer

---
+++ app/src/main/java/com/example/wqms/navigation/ui/RateApp/RateApp.java
@@ -0,0 +1,38 @@
+package com.example.wqms.navigation.ui.RateApp;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wqms.R;
+
+public class RateApp extends Fragment {
+
+    private RateAppViewModel mViewModel;
+
+    public static RateApp newInstance() {
+        return new RateApp();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.rate_app_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RateAppViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/wqms/navigation/ui/RateApp/RateAppViewModel.java
@@ -0,0 +1,7 @@
+package com.example.wqms.navigation.ui.RateApp;
+
+import androidx.lifecycle.ViewModel;
+
+public class RateAppViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
